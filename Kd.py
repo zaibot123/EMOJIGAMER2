aca26cd3755f77be3e6c3a8d7f558b6fae516f75
Gome <82088992+zaibot123@users.noreply.github.com>
2023-08-16 13:44:44 +0200

    Add files via upload

--- Kd.py
+++ Kd.py
@@ -10,6 +10,21 @@ lower_red = np.array([0, 0, 0], dtype = "uint8")
 upper_red= np.array([255, 255, 255], dtype = "uint8")
 height=0
 width=0
+monitor=""
+found=False
+for m in get_monitors():
+    height=m.height
+    width=m.width
+
+def search(img):
+    rows, cols, _ = img.shape
+    for x in range(rows):
+        for y in range(cols):
+            px = list(img[x, y])
+            if px == [0, 255, 0]:
+                    print("GRØN")
+                    win32api.SetCursorPos((y,x))
+                    return
 
 
 with mss() as sct:
@@ -18,7 +33,7 @@ with mss() as sct:
         monitor = sct.monitors[monitor_number]
         print(monitor)
         
-        mon = {'left': 0, 'top': 0, 'width': width, 'height': height}
+        mon = {'left': 0, 'top': 0, 'width': width, 'height': height, "mon":monitor}
         screenShot = sct.grab(mon)
         img = Image.frombytes(
             'RGB', 
@@ -26,14 +41,12 @@ with mss() as sct:
             screenShot.rgb, 
         )
 
-        rows, cols, _ = img.shape
-        for x in range(rows):
-            
-            for y in range(cols):
-                px = list(img[x, y])
-                if px == [0, 255, 0]:
-                    print("GRØN")
-                    win32api.SetCursorPos((y,x))
+        img=np.array(img)
+        search(img)
+
+
+        #cv2.imshow("red color detection", detected_output) 
+   
 
 
 
