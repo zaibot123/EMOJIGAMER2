bca1997ddfb534e00209bf23440b74f876e283f9
Gome <82088992+zaibot123@users.noreply.github.com>
2023-08-16 13:27:58 +0200

    Add files via upload

--- Kd.py
+++ Kd.py
@@ -3,16 +3,13 @@ import cv2
 from screeninfo import get_monitors
 from mss import mss
 from PIL import Image
+import win32api, win32con
 
 lower_red = np.array([0, 0, 0], dtype = "uint8") 
 
 upper_red= np.array([255, 255, 255], dtype = "uint8")
 height=0
 width=0
-monitor=""
-for m in get_monitors():
-    height=m.height
-    width=m.width
 
 
 with mss() as sct:
@@ -21,7 +18,7 @@ with mss() as sct:
         monitor = sct.monitors[monitor_number]
         print(monitor)
         
-        mon = {'left': 0, 'top': 0, 'width': width, 'height': height, "mon":monitor}
+        mon = {'left': 0, 'top': 0, 'width': width, 'height': height}
         screenShot = sct.grab(mon)
         img = Image.frombytes(
             'RGB', 
@@ -29,14 +26,18 @@ with mss() as sct:
             screenShot.rgb, 
         )
 
-        img=np.array(img)
-        mask = cv2.inRange(img, lower_red, upper_red)
+        rows, cols, _ = img.shape
+        for x in range(rows):
+            
+            for y in range(cols):
+                px = list(img[x, y])
+                if px == [0, 255, 0]:
+                    print("GRØN")
+                    win32api.SetCursorPos((y,x))
 
-        detected_output = cv2.bitwise_and(img, img, mask =  mask) 
- 
 
-        cv2.imshow("red color detection", detected_output) 
-        print(detected_output)
+
+        #print(detected_output)
        
         if cv2.waitKey(33) & 0xFF in (
             ord('q'), 
